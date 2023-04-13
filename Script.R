5fef4b90171bb1b4772cd6de5802e06afaea3cca
Faisal <faisalraiyan.h3@hotmail.com>
2023-04-13 12:38:47 -0700

    All EDA

--- Script.R
+++ Script.R
@@ -8,17 +8,24 @@ DF = read.csv('./Data/insurance.csv')
 str(DF)
 plot(DF)
 
+#Fix the outliers first
+
+
+
 #EDA for smoker
 boxplot(charges~smoker, data=DF)
 ggplot(data=DF, mapping = aes(x=smoker,y=charges,fill=smoker))+geom_boxplot()
 
 #Charges and children
-ggplot(data=DF, mapping = aes(x=as.factor(children), y=charges,fill=children))+geom_boxplot()
+ggplot(data=DF, mapping = aes(x=as.factor(children), y=charges))+geom_boxplot()
 
 #Charges and sex
-
+ggplot(data=DF, mapping = aes(x=sex,y=charges,fill=sex))+geom_boxplot()
 
 #Charges and region
+ggplot(data=DF, mapping = aes(x=region,y=charges))+geom_boxplot()
 
+#Charges with sex and status of smoker
+ggplot(data=DF, mapping = aes(x=sex,y=charges,fill=smoker))+geom_boxplot()
 
 

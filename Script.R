9a671400c39a2c38fb528ce3094de643b99ac759
Faisal <faisalraiyan.h3@hotmail.com>
2023-04-13 12:20:51 -0700

    EDA

--- Script.R
+++ Script.R
@@ -11,3 +11,14 @@ plot(DF)
 #EDA for smoker
 boxplot(charges~smoker, data=DF)
 ggplot(data=DF, mapping = aes(x=smoker,y=charges,fill=smoker))+geom_boxplot()
+
+#Charges and children
+ggplot(data=DF, mapping = aes(x=as.factor(children), y=charges,fill=children))+geom_boxplot()
+
+#Charges and sex
+
+
+#Charges and region
+
+
+

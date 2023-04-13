8dc21288a7a69084ba4f8e846ac00bcccbe7e8d8
Faisal <faisalraiyan.h3@hotmail.com>
2023-04-12 23:43:54 -0700

    Save

--- Script.R
+++ Script.R
@@ -1,2 +1,13 @@
+library(ggplot2)
+
 DF = read.csv('./Data/insurance.csv')
 
+#EDA of the dataset
+#Plan so far 
+
+str(DF)
+plot(DF)
+
+#EDA for smoker
+boxplot(charges~smoker, data=DF)
+ggplot(data=DF, mapping = aes(x=smoker,y=charges,fill=smoker))+geom_boxplot()
